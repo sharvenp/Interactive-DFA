34f2b2e45e817723db02ac190378d0ab3689997b
sharvenp <sharven.dhanasekar@mail.utoronto.ca>
2019-12-07 02:35:32 -0500

    Added settings class to handle or parametric settings

---
+++ src/settings.py
@@ -0,0 +1,25 @@
+
+import pygame as pg
+
+class Settings:
+
+	dimensions = 700
+
+	# Color Pallete
+	BACKGROUND_COLOR = (0, 0, 0)
+	STATE_COLOR = (255, 255, 255)
+	SELECTED_COLOR = (255, 150, 150)
+	ACCEPTING_COLOR = (180, 255, 180)
+	EDGE_COLOR = (255, 255, 255)
+
+	# Fonts
+	DFA_FONT = ('Consolas', 25)
+
+
+	# State Parameters
+	STATE_RADIUS = 40
+	STATE_DEFAULT_THICKNESS = 2
+	STATE_ACCEPTING_THICKNESS = 5
+
+	# Edge Parameters
+	EDGE_THICKNESS = 3
